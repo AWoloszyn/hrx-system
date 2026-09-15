73a300a6a4511cabf113ef95aceb6e949053eeda
Andrew Woloszyn <andrew.woloszyn@gmail.com>
2026-09-15 10:35:36 -0400

    [HIP] Keep data corruption out of fatal fault state
    Reserve the abort status used by asynchronous device delivery for memory access faults, which are the only failures that latch hipErrorIllegalAddress.
    
    Report hardware and queue exceptions as data loss and translate them to hipErrorLaunchFailure so malformed images and other data errors cannot poison the process as illegal addresses.

--- runtime/src/iree/hal/drivers/amdgpu/util/libhsa_test.cc
+++ runtime/src/iree/hal/drivers/amdgpu/util/libhsa_test.cc
@@ -14,12 +14,12 @@ namespace iree::hal::amdgpu {
 namespace {
 
 TEST(LibHSATest, DeviceFailureStatusesPreserveCause) {
-  IREE_EXPECT_STATUS_IS(IREE_STATUS_ABORTED,
+  IREE_EXPECT_STATUS_IS(IREE_STATUS_DATA_LOSS,
                         iree_status_from_hsa_status(__FILE__, __LINE__,
                                                     HSA_STATUS_ERROR_EXCEPTION,
                                                     "exception", nullptr));
   IREE_EXPECT_STATUS_IS(
-      IREE_STATUS_ABORTED,
+      IREE_STATUS_DATA_LOSS,
       iree_status_from_hsa_status(__FILE__, __LINE__, HSA_STATUS_ERROR_FATAL,
                                   "fatal", nullptr));
 }
